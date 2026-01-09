1484bcef7729122030ae9d2da72d36e5d6438bdc
theom4 <154817034+theom4@users.noreply.github.com>
2026-01-09 19:24:07 +0200

    feat: Configure Supabase client for session persistence
    Enhance Supabase client initialization with options to persist sessions, automatically refresh tokens, and detect sessions in the URL. This ensures a more robust and seamless user authentication experience.

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -11,4 +11,10 @@ const getEnvVar = (key: string) => {
 
 const supabaseUrl = 'https://ympkyaakwveogjcgqqnr.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltcGt5YWFrd3Zlb2dqY2dxcW5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxNjkyMDcsImV4cCI6MjA3NTc0NTIwN30.OtkJR9sK5wgUKyXAYpHM99ddA-3sbleUN9WLVRVSgZA';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true
+  }
+});
